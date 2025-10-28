5c3d6294511b2c5cf4f4137a4e43a2a44e809dc9
Jiang <jiangzhiguo1992@163.com>
2025-10-28 20:10:12 +0800

    fix

--- pkg/validator/v5/factory.go
+++ pkg/validator/v5/factory.go
@@ -1,8 +1,17 @@
 package v5
 
+import (
+	"katydid-common-account/pkg/validator/v5/core"
+	"katydid-common-account/pkg/validator/v5/engine"
+	"katydid-common-account/pkg/validator/v5/formatter"
+	"katydid-common-account/pkg/validator/v5/registry"
+	"katydid-common-account/pkg/validator/v5/strategy"
+
+	"github.com/go-playground/validator/v10"
+)
+
 // ValidatorFactory 验证器工厂
 // 职责：创建预配置的验证器实例
-// 设计模式：工厂模式
 type ValidatorFactory struct{}
 
 // NewValidatorFactory 创建验证器工厂
@@ -11,29 +20,28 @@ func NewValidatorFactory() *ValidatorFactory {
 }
 
 // Create 创建自定义验证器
-func (f *ValidatorFactory) Create(opts ...EngineOption) *ValidatorEngine {
-	return NewValidatorEngine(opts...)
+func (f *ValidatorFactory) Create(opts ...engine.ValidatorEngineOption) core.IValidator {
+	v := validator.New()
+	typeRegistry := registry.NewTypeRegistry(v)
+	return engine.NewValidatorEngine(typeRegistry, opts...)
 }
 
 // CreateDefault 创建默认验证器
-func (f *ValidatorFactory) CreateDefault() *ValidatorEngine {
-	engine := NewValidatorEngine()
+func (f *ValidatorFactory) CreateDefault() core.IValidator {
+	v := validator.New()
+	typeRegistry := registry.NewTypeRegistry(v)
+	sceneMatcher := core.NewSceneBitMatcher()
+	maxDepth := int8(100)
+
+	ve := engine.NewValidatorEngine(typeRegistry,
+		engine.WithStrategies(strategy.NewRuleStrategy(v, typeRegistry, sceneMatcher)),
+		engine.WithStrategies(strategy.NewBusinessStrategy()),
+		engine.WithErrorFormatter(formatter.NewLocalizesErrorFormatter()),
+		engine.WithMaxDepth(maxDepth))
 
 	// 添加标准策略
-	engine.AddStrategy(NewRuleStrategy(engine.validator, engine.sceneMatcher, engine.registry))
-	engine.AddStrategy(NewBusinessStrategy())
-	engine.AddStrategy(NewNestedStrategy(engine, engine.maxDepth))
+	ve.AddStrategy(strategy.NewNestedStrategy(ve, maxDepth))
 
 	// 没有listener
-	return engine
-}
-
-// CreateMinimal 创建最小验证器
-func (f *ValidatorFactory) CreateMinimal() *ValidatorEngine {
-	engine := NewValidatorEngine()
-
-	// 只包含规则验证策略
-	engine.AddStrategy(NewRuleStrategy(engine.validator, engine.sceneMatcher, engine.registry))
-
-	return engine
+	return ve
 }
